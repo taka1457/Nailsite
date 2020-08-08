693df21e3775efb2c09ae5d2ed249a6f3bc25e18
vagrant <vagrant@localhost.localdomain>
2020-08-08 14:34:44 +0000

    [Add]header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :shops
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope module: :public do
+  	root 'homes#top'
+  	get 'about', to: 'homes#about'
+  end
 end
