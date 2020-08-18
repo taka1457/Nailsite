3b3525a8126c8425497bdea6ea75a347c0da4ecf
vagrant <vagrant@localhost.localdomain>
2020-08-18 16:48:43 +0000

    [Fix]history_comment_ajax

--- app/controllers/public/history_comments_controller.rb
+++ app/controllers/public/history_comments_controller.rb
@@ -1,25 +1,23 @@
 class Public::HistoryCommentsController < ApplicationController
 	def create
+    @reservation_histories = ReservationHistory.page(params[:page]).reverse_order.per(10)
     @reservation_history = ReservationHistory.find(params[:reservation_history_id])
     @history_comment = @reservation_history.history_comment.new(history_comment_params)
     @history_comment.customer_id = current_customer.id
     if @history_comment.save
-      flash[:success] = "Comment was successfully created."
-      redirect_to request.referer
     else
       @reservation_history_new = Post.new
       @history_comments = @reservation_history.history_comments
-      render '/posts/show'
     end
   end
 
   def destroy
+    @reservation_histories = ReservationHistory.page(params[:page]).reverse_order.per(10)
     @reservation_history = ReservationHistory.find(params[:reservation_history_id])
     @history_comment = HistoryComment.find(params[:id])
     if @history_comment.customer == current_customer
       @history_comment.destroy
     end
-    redirect_to request.referer
   end
 
   private
