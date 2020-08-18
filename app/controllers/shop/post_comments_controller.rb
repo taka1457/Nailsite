d3b22600fab6385fbd68c4318a1e9b0deae7a2c3
vagrant <vagrant@localhost.localdomain>
2020-08-18 14:50:31 +0000

    [Fix]comment_ajax

--- app/controllers/shop/post_comments_controller.rb
+++ app/controllers/shop/post_comments_controller.rb
@@ -3,25 +3,24 @@ class Shop::PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    @shop = Shop.find(params[:shop_id])
     @post_comment = @post.post_comments.new(post_comment_params)
     @post_comment.customer_id = current_customer.id
     if @post_comment.save
       flash[:success] = "Comment was successfully created."
-      redirect_to request.referer
     else
       @post_new = Post.new
       @post_comments = @post.post_comments
-      render '/posts/show'
     end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    @shop = Shop.find(params[:shop_id])
     @post_comment = PostComment.find(params[:id])
     if @post_comment.customer == current_customer
       @post_comment.destroy
     end
-    redirect_to request.referer
   end
 
   private
