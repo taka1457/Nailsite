5c49417f59f5aa04d470056afcbbe15597954cef
vagrant <vagrant@localhost.localdomain>
2020-08-25 14:05:07 +0000

    [Fix]search_controller

--- app/controllers/public/searchs_controller.rb
+++ app/controllers/public/searchs_controller.rb
@@ -1,12 +1,12 @@
 class Public::SearchsController < ApplicationController
   def search
     @content = params["search"]["content"]
-    @records = search_for(@content).page(params[:page]).per(4)
+    @records = search_for(@content).page(params[:page]).per(20)
   end
 
   private
 
   def search_for(content)
-    Shop.where('(name LIKE ?) OR (prefecture_code LIKE ?) OR (city LIKE ?) OR (street LIKE ?)', '%' + content + '%', '%' + content + '%', '%' + content + '%', '%' + content + '%')
+    Shop.where('(name LIKE ?) OR (prefecture_code LIKE ?) OR (city LIKE ?) OR (street LIKE ?) OR (promotion LIKE ?) OR (introduction LIKE ?)', '%' + content + '%', '%' + content + '%', '%' + content + '%', '%' + content + '%', '%' + content + '%', '%' + content + '%')
   end
 end
