b357088ccfa8fbb4193f7dd81a70b3fb55ac8b26
vagrant <vagrant@localhost.localdomain>
2020-08-25 05:58:11 +0000

    [Fix]post_rank

--- app/controllers/shop/posts_controller.rb
+++ app/controllers/shop/posts_controller.rb
@@ -6,7 +6,7 @@ class Shop::PostsController < ApplicationController
   end
 
   def rank
-    @posts = Kaminari.paginate_array(Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))).page(params[:page]).per(9)
+    @posts = Kaminari.paginate_array(Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))).page(params[:page]).per(9)
   end
 
   def show
