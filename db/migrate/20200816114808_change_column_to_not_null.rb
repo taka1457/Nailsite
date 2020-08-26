167f0e532fbb937dc377176458147d1b05e82b1a
vagrant <vagrant@localhost.localdomain>
2020-08-26 03:04:40 +0000

    [Fix]migration

--- db/migrate/20200816114808_change_column_to_not_null.rb
+++ db/migrate/20200816114808_change_column_to_not_null.rb
@@ -1,6 +1,5 @@
 class ChangeColumnToNotNull < ActiveRecord::Migration[5.2]
   def change
-  	change_column :talks, :customer_id ,:integer, null: true
   	change_column :talks, :shop_id ,:integer, null: true
   end
 end
