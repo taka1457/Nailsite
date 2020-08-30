53beb7f0c9e7ab36c4b555b3a6271e27c43b309b
vagrant <vagrant@localhost.localdomain>
2020-08-30 07:13:54 +0000

    [Update]shop_talk_controller

--- app/controllers/shop/talks_controller.rb
+++ app/controllers/shop/talks_controller.rb
@@ -14,8 +14,9 @@ class Shop::TalksController < ApplicationController
     shop_rooms = ShopRoom.find_by(shop_id: @shop.id, talk_room_id: talk_rooms)
     @talk_room = shop_rooms.talk_room
     @talks = Talk.where(shop_id: @shop.id).where(customer_id: current_customer).reverse_order
-    @talk = Talk.new(talk_room_id: @talk_room.id)
-    if @talk.customer_id != current_customer
+    if @talks.present?
+      @talk = Talk.new(talk_room_id: @talk_room.id)
+    else
       redirect_to shops_path
     end
   end
