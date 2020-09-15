d29b3430edc969c36382de74ba9dd41ceff4d4f2
vagrant <vagrant@localhost.localdomain>
2020-09-15 08:16:58 +0000

    [Add]RSpec_factories_custoemr

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Postモデルのテスト', type: :model do
-    describe 'バリデーションのテスト' do
-      let(:shop) { FactoryBot.create(:shop) }
-      let!(:post) { build(:post, shop_id: shop.id) }
-  
-      context 'bodyカラム' do
-        it '140文字以下であること' do
-          post.body = Faker::Lorem.characters(number:141)
-          expect(post.valid?).to eq false;
-        end
-      end
-      context 'bodyカラム' do
-        it '141文字以上だとvalidationエラーを表示' do
-          post.body = Faker::Lorem.characters(number:141)
-          expect(post.valid?).to eq false;
-        end
+  describe 'バリデーションのテスト' do
+    let(:shop) { FactoryBot.create(:shop) }
+    let!(:post) { build(:post, shop_id: shop.id) }
+
+    context 'bodyカラム' do
+      it '140文字以下であること' do
+        post.body = Faker::Lorem.characters(number:141)
+        expect(post.valid?).to eq false;
       end
     end
-    describe 'アソシエーションのテスト' do
-      context 'Shopモデルとの関係' do
-        it 'N:1となっている' do
-          expect(Post.reflect_on_association(:shop).macro).to eq :belongs_to
-        end
+    context 'bodyカラム' do
+      it '141文字以上だとvalidationエラーを表示' do
+        post.body = Faker::Lorem.characters(number:141)
+        expect(post.valid?).to eq false;
       end
     end
   end
+  describe 'アソシエーションのテスト' do
+    context 'Shopモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:shop).macro).to eq :belongs_to
+      end
+    end
+  end
+end
